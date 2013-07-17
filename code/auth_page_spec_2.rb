df572212538886dfa15707b72703c74315aff5a5
Aleksandrs Zdancuks <aleksandrs.zdancuks@accenture.com>
2013-07-17 12:52:50 +0300

    Updated to use correct version and so on

---
+++ code/auth_page_spec_2.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "Authentication" do
+ ...
+
+  describe "signin" do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_selector('title', text: 'Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+    end
+  end
+end
